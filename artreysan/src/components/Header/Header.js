e179ec75ef1cb250148023e4100d7d191ce20b6f
artreysan <arthur.rs0313@gmail.com>
2025-04-08 18:30:40 -0600

    init: new portfolio

---
+++ artreysan/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Arturo Reyes Santana</h1>
+      <p>DEV</p>
+    </header>
+  );
+};
+
+export default Header;
